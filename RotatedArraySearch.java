fd6d0e446c02feab02c34fe833cc5bd75c0ddefd
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-10-18 21:37:56 +0530

    Add files via upload

---
+++ RotatedArraySearch.java
@@ -0,0 +1,44 @@
+public class RotatedArraySearch {
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                // The left half is sorted
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // The right half is sorted
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] rotatedArray = {4, 5, 6, 7, 0, 1, 2};
+        int target = 5;
+        int result = search(rotatedArray, target);
+
+        if (result != -1) {
+            System.out.println("Target " + target + " found at index " + result);
+        } else {
+            System.out.println("Target " + target + " not found in the array.");
+        }
+    }
+}

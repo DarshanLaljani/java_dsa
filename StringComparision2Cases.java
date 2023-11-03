cb97f5db6d9cb8545c1beccf6b03004de23b7b48
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-11-03 21:47:31 +0530

    Add files via upload

---
+++ StringComparision2Cases.java
@@ -0,0 +1,13 @@
+public class StringComparision2Cases {
+    public static void main(String[] args) {
+
+
+        String a = "Darshan";
+        String b = "Darshan";
+        System.out.println(a==b);
+
+        String a1 = new String("Darshan");
+        String b1 = new String("Darshan");
+        System.out.println(a1==b1);
+    }
+}

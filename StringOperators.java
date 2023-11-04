e445a6a2578702f5ee70765e5594196ae3174526
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-11-04 22:56:39 +0530

    Add files via upload

---
+++ StringOperators.java
@@ -0,0 +1,14 @@
+public class StringOperators {
+    public static void main(String[] args) {
+        System.out.println('a'+'b');
+        System.out.println("a"+"b");
+        System.out.println((char)('a'+3));
+        System.out.println("a" +1);
+
+    }
+    // Outputs for the above operations
+//195
+//ab
+//d
+//a1
+}

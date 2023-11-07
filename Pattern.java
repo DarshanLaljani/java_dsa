81cdbc5da08fd55f8a391b313d7e4fcb30f8dfea
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-11-07 21:56:40 +0530

    Add files via upload
    3 patterns

---
+++ Pattern.java
@@ -0,0 +1,36 @@
+public class Pattern {
+    public static void main(String[] args) {
+        pattern1(4);
+        pattern2(4);
+        pattern3(4);
+    }
+
+    static void pattern1(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+    static void pattern3(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n-row+1; col++) {
+                System.out.print("* ");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+}

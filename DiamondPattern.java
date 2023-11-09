131eb11309d1e4eb3c6b4b549724937eccd44b96
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-11-09 22:07:21 +0530

    Add files via upload

---
+++ DiamondPattern.java
@@ -0,0 +1,19 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        pattern6(4);
+    }
+    static void pattern6(int n){
+        for (int row = 0; row <2*n ; row++) {
+            int totalColsInRow = row>n?2 *n -row:row;
+            int noOfSpace = n-totalColsInRow;
+            for (int s = 0; s < noOfSpace; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}

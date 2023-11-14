2037731b1b94c003a806d80ba10eda494ce3efe3
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-11-14 23:30:14 +0530

    Add files via upload

---
+++ Rec_BinarySearch.java
@@ -0,0 +1,20 @@
+public class Rec_BinarySearch {
+    public static void main(String[] args) {
+    int[] arr = {1,4,6,7,8,9,13};
+        System.out.println(search(arr,7,0, arr.length-1));
+    }
+    static int search(int[] arr , int target, int s, int e){
+        if(s>e){
+            return -1;
+        }
+        int m = s+(e-s)/2;
+
+        if(arr[m]==target){
+            return m;
+        }
+        if (target<arr[m]){
+           return search(arr,target,s, m-1);
+        }
+        return search(arr, target,m+1,e);
+    }
+}

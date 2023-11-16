603d46401a31c9aab6cfa5828f8b5bb71e2af664
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-11-16 23:26:39 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial {
+    public static void main(String[] args) {
+       int ans= factorial(4);
+        System.out.println(ans);
+    }
+    public static int factorial(int n){
+
+        if (n<=1){
+            return 1;
+
+        }
+        return n*factorial(n-1);
+
+    }
+}

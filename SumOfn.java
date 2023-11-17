8cbae7bf289495d0e7db1d7b4b183f4326eba7da
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-11-17 23:43:32 +0530

    Add files via upload

---
+++ SumOfn.java
@@ -0,0 +1,16 @@
+public class SumOfn {
+    public static void main(String[] args) {
+        int ans= sum(4);
+        System.out.println(ans);
+    }
+    public static int sum(int n){
+
+        if (n<=1){
+            return 1;
+
+        }
+        return n+ sum(n-1);
+
+    }
+
+}

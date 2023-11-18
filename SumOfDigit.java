4375a074e5520adcde6bef83959b3edb7659bc2e
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2023-11-18 20:31:16 +0530

    Add files via upload

---
+++ SumOfDigit.java
@@ -0,0 +1,13 @@
+public class SumOfDigit {
+    public static void main(String[] args) {
+            int ans = sum(51);
+        System.out.println(ans);
+    }
+    public static int sum(int n){
+        if (n==0){
+            return 0;
+        }
+            return (n%10) + sum(n/10);
+    }
+
+}

f341c38c20d86b8a1e3ea057a02d3082747e5e2b
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2024-01-18 22:46:11 +0530

    Add files via upload

---
+++ Maze.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Maze {
+    public static void main(String[] args) {
+        path("",3,3);
+        System.out.println(retPath("",3,3));
+    }
+
+    static int count(int r , int c){
+        if (r==1 || c==1){
+            return 1;
+        }
+        int down = count(r-1,c);
+        int right = count(r,c-1);
+        return down+right;
+    }
+    static void path(String p , int r , int c){
+        if (r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+        if (r>1){
+            path(p+'D',r-1,c);
+        }
+        if (c>1){
+            path(p+'R',r,c-1);
+        }
+    }
+    static ArrayList<String> retPath(String p , int r , int c){
+        if (r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (r>1){
+            list.addAll(retPath(p+'D',r-1,c));
+        }
+        if (c>1){
+          list.addAll(retPath(p+'R',r,c-1));
+        }
+        return list;
+    }
+}

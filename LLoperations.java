d15a275446c6ddd32127caf328b3f7a672ba2c9d
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2024-01-25 22:11:36 +0530

    Add files via upload

---
+++ LLoperations.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class LLoperations {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(18);
+        list.insert(100,3);
+        list.insertLast(99);
+        list.display();
+        list.deleteFirst();
+        list.deleteLast();
+        list.display();
+    }
+}

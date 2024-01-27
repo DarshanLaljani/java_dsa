0ea58ab34a260f9914daac4ebcb162a0c3266861
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2024-01-27 22:52:42 +0530

    Add files via upload

---
+++ DLLoperations.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class DLLoperations {
+    public static void main(String[] args) {
+        DoublyLL list1 = new DoublyLL();
+        list1.insertFirstDD(3);
+        list1.insertFirstDD(4);
+        list1.insertFirstDD(24);
+        list1.insertFirstDD(43);
+
+        list1.display();
+    }
+
+}

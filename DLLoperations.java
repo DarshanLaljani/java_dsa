5e4ca3bc0319061d273cf6dbee7fd23293f56984
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2024-01-28 22:58:02 +0530

    Add files via upload

--- DLLoperations.java
+++ DLLoperations.java
@@ -7,8 +7,9 @@ public class DLLoperations {
         list1.insertFirstDD(4);
         list1.insertFirstDD(24);
         list1.insertFirstDD(43);
-
+        list1.insertionLast(99);
         list1.display();
+
     }
 
 }

9d15a89af9971e25a38a88f4075c44480e14cb4d
Darshan Laljani <124714422+DarshanLaljani@users.noreply.github.com>
2024-01-28 22:57:45 +0530

    Add files via upload

--- DoublyLL.java
+++ DoublyLL.java
@@ -2,6 +2,7 @@ package LinkedList;
 
 public class DoublyLL {
     private Node head;
+    private Node tail;
     public void insertFirstDD(int val){
         Node node = new Node(val);
         node.next=head;
@@ -14,12 +15,37 @@ public class DoublyLL {
     }
     public void display(){
         Node node = head;
+        Node last = null;
         while(node!=null){
             System.out.print(node.val+ "->");
+            last=node;
             node = node.next;
-
         }
         System.out.println("END");
+
+        while(last!=null){
+            System.out.print(last.val+ "->");
+            last = last.prev;
+        }
+        System.out.println("END");
+    }
+    public void insertionLast(int val){
+
+        Node node = new Node(val);
+        Node last = head;
+        node.next=null;
+        if (head==null){
+            node.prev=null;
+            head = node;
+            return;
+        }
+        while (last.next!= null){
+            last=last.next;
+        }
+        last.next=node;
+        node.prev=last;
+
+
     }
     class Node {
       int val;
